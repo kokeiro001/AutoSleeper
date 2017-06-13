445e5caa96c0906c2ffbbce9d0b8b4e61ae2ecf1
kokeiro <kokeiro.ssbm.001@gmail.com>
2017-06-13 23:10:21 +0900

    タスクトレイに常駐する機能を実装した

---
+++ AutoSleeper.App/NotifyIconWrapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AutoSleeper.App
+{
+    public partial class NotifyIconWrapper : Component
+    {
+          
+        public NotifyIconWrapper()
+        {
+            InitializeComponent();
+
+            toolStripMenuItem_Open.Click += ToolStripMenuItem_Open_Click;
+            toolStripMenuItem_Exit.Click += ToolStripMenuItem_Exit_Click;
+        }
+
+        public NotifyIconWrapper(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+
+        }
+
+        private void ToolStripMenuItem_Open_Click(object sender, EventArgs e)
+        {
+            var window = new MainWindow();
+            window.Show();
+        }
+
+        private void ToolStripMenuItem_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+    }
+}
